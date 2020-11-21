c83781467c9aa10e3207f668199391354e124f49
Pedro Barata <pedrofelipebarata@gmail.com>
2020-11-20 21:20:22 -0300

    feat: TDD

--- TDD-Junit/teste-de-unidade/src/br/com/caelum/leilao/dominio/Leilao.java
+++ TDD-Junit/teste-de-unidade/src/br/com/caelum/leilao/dominio/Leilao.java
@@ -7,26 +7,49 @@ import java.util.List;
 
 public class Leilao {
 
-	private String descricao;
-	private List<Lance> lances;
-	
-	public Leilao(String descricao) {
-		this.descricao = descricao;
-		this.lances = new ArrayList<Lance>();
-	}
-	
-	public void propoe(Lance lance) {
-		lances.add(lance);
-	}
+    private String descricao;
+    private List<Lance> lances;
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public Leilao(String descricao) {
+        this.descricao = descricao;
+        this.lances = new ArrayList<Lance>();
+    }
 
-	public List<Lance> getLances() {
-		return Collections.unmodifiableList(lances);
-	}
+    public void propoe(Lance lance) {
+        if (this.getLances().isEmpty() || podeDarLance(lance)) {
+            lances.add(lance);
+        }
+    }
 
-	
-	
+    public boolean podeDarLance(Lance lance) {
+        return !doisLancesSeguidosDoMesmoUsuario(lance) && totalDeLancesDo(lance.getUsuario()) < 5;
+    }
+
+    private int totalDeLancesDo(Usuario usuario) {
+        int total = 0;
+        for (Lance l : this.getLances())
+            if (l.getUsuario().equals(usuario)) {
+                total++;
+            }
+        return total;
+    }
+
+
+    private Lance ultimoLanceDado() {
+        return this.getLances().get(this.getLances().size() - 1);
+    }
+
+    private boolean doisLancesSeguidosDoMesmoUsuario(Lance lance) {
+        return lance.getUsuario().equals(ultimoLanceDado().getUsuario());
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public List<Lance> getLances() {
+        return Collections.unmodifiableList(lances);
+    }
+
+
 }
---
+++ TDD-Junit/teste-de-unidade/src/test/servico/dominio/LeilaoTest.java
@@ -0,0 +1,65 @@
+package test.servico.dominio;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeilaoTest {
+
+    @Test
+    public void naoDeveAceitarDoisLancesSeguidosDoMesmoUsuario() {
+        Usuario joao = new Usuario("joao");
+
+        Leilao leilao = new Leilao("Playstation");
+
+        leilao.propoe(new Lance(joao, 250.0));
+        leilao.propoe(new Lance(joao, 450.0));
+
+        assertEquals(1, leilao.getLances().size());
+        assertEquals(250.0, leilao.getLances().get(0).getValor());
+
+    }
+
+    @Test
+    public void naoDeveAceitarMaisDoQue5LancesDeUmMesmoUsuario() {
+        Usuario joao = new Usuario("joao");
+        Usuario jose = new Usuario("jose");
+
+        Leilao leilao = new Leilao("Playstation");
+
+        leilao.propoe(new Lance(joao, 250.0));
+        leilao.propoe(new Lance(jose, 450.0));
+
+        leilao.propoe(new Lance(joao, 500.0));
+        leilao.propoe(new Lance(jose, 600.0));
+
+        leilao.propoe(new Lance(joao, 700.0));
+        leilao.propoe(new Lance(jose, 1000.0));
+
+        leilao.propoe(new Lance(joao, 800.0));
+        leilao.propoe(new Lance(jose, 2000.0));
+
+        leilao.propoe(new Lance(joao, 1400.0));
+        leilao.propoe(new Lance(jose, 3000.0));
+
+        //Nao pode aceitar
+        leilao.propoe(new Lance(joao, 5000.0));
+
+        assertEquals(10, leilao.getLances().size());
+        assertEquals(250.0, leilao.getLances().get(0).getValor());
+        assertEquals(450.0, leilao.getLances().get(1).getValor());
+        assertEquals(500.0, leilao.getLances().get(2).getValor());
+        assertEquals(600.0, leilao.getLances().get(3).getValor());
+        assertEquals(700.0, leilao.getLances().get(4).getValor());
+        assertEquals(1000.0, leilao.getLances().get(5).getValor());
+        assertEquals(800.0, leilao.getLances().get(6).getValor());
+        assertEquals(2000.0, leilao.getLances().get(7).getValor());
+        assertEquals(1400.0, leilao.getLances().get(8).getValor());
+        assertEquals(3000.0, leilao.getLances().get(9).getValor());
+
+    }
+
+}
