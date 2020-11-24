cdde948175b798f28194ec75ebc95c8056be3d30
Pedro Barata <pedrofelipebarata@gmail.com>
2020-11-23 22:51:15 -0300

    feat: More auction tests

--- pm73-dao/test/br/com/caelum/pm73/dao/LeilaoDaoTest.java
+++ pm73-dao/test/br/com/caelum/pm73/dao/LeilaoDaoTest.java
@@ -8,6 +8,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class LeilaoDaoTest {
     private Session session;
     private UsuarioDao usuarioDao;
@@ -63,5 +67,73 @@ public class LeilaoDaoTest {
 
         //validacoes
         Assert.assertEquals(0L, total);
+    }
+
+    @Test
+    public void deveRetornarApenasLeiloesNovos() {
+        //cenario
+        Usuario mauricio = new Usuario("mauricio", "mauricio@mauricio.com.br");
+
+        Leilao leilao = new Leilao("Geladeira", 1500.0, mauricio, false);
+        Leilao encerrado = new Leilao("Torradeira", 100.0, mauricio, true);
+        encerrado.encerra();
+        leilao.encerra();
+        //acoes
+        leilaoDao.salvar(leilao);
+        leilaoDao.salvar(encerrado);
+        usuarioDao.salvar(mauricio);
+        List<Leilao> novosLeiloes = leilaoDao.novos();
+
+        //validacoes
+        Assert.assertEquals(1, novosLeiloes.size());
+        Assert.assertEquals("Geladeira", novosLeiloes.get(0).getNome());
+
+    }
+
+    @Test
+    public void deveRetornarApenasLeiloesDeUmaSemana() {
+        Calendar dataAntiga = Calendar.getInstance();
+        Calendar dataAtual = Calendar.getInstance();
+        dataAntiga.add(Calendar.DAY_OF_MONTH, -10);
+        //cenario
+        Usuario mauricio = new Usuario("mauricio", "mauricio@mauricio.com.br");
+
+        Leilao leilao = new Leilao("Geladeira", 1500.0, mauricio, false);
+        leilao.setDataAbertura(dataAntiga);
+
+        Leilao encerrado = new Leilao("Torradeira", 100.0, mauricio, true);
+        encerrado.setDataAbertura(dataAtual);
+
+        encerrado.encerra();
+        //acoes
+        leilaoDao.salvar(leilao);
+        leilaoDao.salvar(encerrado);
+        usuarioDao.salvar(mauricio);
+        List<Leilao> novosLeiloes = leilaoDao.antigos();
+
+        //validacoes
+        Assert.assertEquals(1, novosLeiloes.size());
+        Assert.assertEquals("Geladeira", novosLeiloes.get(0).getNome());
+
+    }
+
+    @Test
+    public void deveRetornarApenasLeiloesComExatamenteUmaSemana() {
+        Calendar dataAntiga = Calendar.getInstance();
+        dataAntiga.add(Calendar.DAY_OF_MONTH, -7);
+        //cenario
+        Usuario mauricio = new Usuario("mauricio", "mauricio@mauricio.com.br");
+
+        Leilao leilao = new Leilao("Geladeira", 1500.0, mauricio, false);
+        leilao.setDataAbertura(dataAntiga);
+        //acoes
+        leilaoDao.salvar(leilao);
+        usuarioDao.salvar(mauricio);
+        List<Leilao> novosLeiloes = leilaoDao.novos();
+
+        //validacoes
+        Assert.assertEquals(1, novosLeiloes.size());
+        Assert.assertEquals("Geladeira", novosLeiloes.get(0).getNome());
+
     }
 }
