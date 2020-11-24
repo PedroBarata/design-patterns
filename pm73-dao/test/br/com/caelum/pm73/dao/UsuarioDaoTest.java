6271df44d0080982a35654cb1cec78d707a1a247
Pedro Barata <pedrofelipebarata@gmail.com>
2020-11-23 23:22:05 -0300

    feat: Delete and update using hibernate

--- pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -34,4 +34,49 @@ public class UsuarioDaoTest {
         Assert.assertNull(usuario);
         session.close();
     }
+
+    @Test
+    public void deveDeletarOUsuario() {
+        Session session = new CriadorDeSessao().getSession();
+        Usuario joao = new Usuario("Joao", "joao@dasilva.com.br");
+        UsuarioDao usuarioDao = new UsuarioDao(session);
+        usuarioDao.salvar(joao);
+        usuarioDao.deletar(joao);
+
+        /*IMPORTANTE!!*/
+        session.flush();
+        session.clear();
+
+        Usuario deletado = usuarioDao.porNomeEEmail("Joao", "joao@dasilva.com.br");
+        Assert.assertNull(deletado);
+        session.close();
+    }
+
+    @Test
+    public void deveAlterarUmUsuario() {
+        Session session = new CriadorDeSessao().getSession();
+        UsuarioDao usuarioDao = new UsuarioDao(session);
+
+        Usuario usuario =
+                new Usuario("Mauricio Aniche", "mauricio@aniche.com.br");
+
+        usuarioDao.salvar(usuario);
+
+        usuario.setNome("João da Silva");
+        usuario.setEmail("joao@silva.com.br");
+
+        usuarioDao.atualizar(usuario);
+
+        session.flush();
+
+        Usuario novoUsuario =
+                usuarioDao.porNomeEEmail("João da Silva", "joao@silva.com.br");
+        Assert.assertNotNull(novoUsuario);
+        System.out.println(novoUsuario);
+
+        Usuario usuarioInexistente =
+                usuarioDao.porNomeEEmail("Mauricio Aniche", "mauricio@aniche.com.br");
+        Assert.assertNull(usuarioInexistente);
+
+    }
 }
