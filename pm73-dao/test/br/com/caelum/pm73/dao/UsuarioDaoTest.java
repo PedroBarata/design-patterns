a77577bb3f3502a3e25e6a3c5713a282d6674bcb
Pedro Barata <pedrofelipebarata@gmail.com>
2020-11-23 21:41:53 -0300

    feat: Integration Test

---
+++ pm73-dao/test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -0,0 +1,25 @@
+package br.com.caelum.pm73.dao;
+
+import br.com.caelum.pm73.dominio.Usuario;
+import org.hibernate.Session;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UsuarioDaoTest {
+
+
+    @Test
+    public void deveEncontrarUsuarioPorNomeEEmailMockado() {
+        Session session = new CriadorDeSessao().getSession();
+
+        UsuarioDao usuarioDao = new UsuarioDao(session);
+
+        usuarioDao.salvar(new Usuario("Joao", "joao@dasilva.com.br"));
+
+        Usuario usuario = usuarioDao.porNomeEEmail("Joao", "joao@dasilva.com.br");
+
+        Assert.assertEquals("Joao", usuario.getNome());
+        Assert.assertEquals("joao@dasilva.com.br", usuario.getEmail());
+
+    }
+}
